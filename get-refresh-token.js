5dc045d391f9e29c5669ce624712ecb7b7597ac9
lakaytax-hue <lakaytax@gmail.com>
2025-12-07 23:24:23 -0500

    Update get-refresh-token.js

--- get-refresh-token.js
+++ get-refresh-token.js
@@ -1,9 +1,16 @@
 const { google } = require('googleapis');
 const readline = require('readline');
 
-GOOGLE_OAUTH_CLIENT_ID='987557178118-q0obi1ls9oi9mceql26uqh962v8htcvg.apps.googleusercontent.com';
-GOOGLE_OAUTH_CLIENT_SECRET='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';
-GOOGLE_OAUTH_REDIRECT_URI='http://localhost:3000/oauth2callback';
+
+const GOOGLE_OAUTH_CLIENT_ID = "987557178118-q0ob1ls9oi9mecql26ugh962v8htcvg.apps.googleusercontent.com";
+const GOOGLE_OAUTH_CLIENT_SECRET = "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";
+const GOOGLE_OAUTH_REDIRECT_URI = "http://localhost:3000/oauth2callback";
+
+const oauth2Client = new google.auth.OAuth2(
+GOOGLE_OAUTH_CLIENT_ID,
+GOOGLE_OAUTH_CLIENT_SECRET,
+GOOGLE_OAUTH_REDIRECT_URI
+);
 
 function startAuth() {
 const authUrl = oauth2Client.generateAuthUrl({
